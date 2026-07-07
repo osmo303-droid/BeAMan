7ade8c47015999bef430f5a3a90829a0b1ad6120
osmo303-droid <osmo303@gmail.com>
2026-07-07 21:39:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'be-a-man-v1';
+const urlsToCache = [
+  'index.html',
+  'quotes.json',
+  'privacy.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
